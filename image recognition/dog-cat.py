352537d080d70e477bf1cca8d05045b43de2b378
mayur <mayurpatkar68@gmail.com>
2024-02-26 20:06:49 +0530

    update

--- image recognition/dog-cat.py
+++ image recognition/dog-cat.py
@@ -4,37 +4,8 @@ import os
 import cv2
 import numpy as np
 
-train_dir = 'D:\machine learning\Machine-learning-Models\image recognition\dataset'
-test_dir = 'D:\machine learning\Machine-learning-Models\image recognition\dataset'
-
-train_images = []
-train_labels = []
-test_images = []
-test_labels = []
-
-def read_and_preprocess_images(directory):
-    images = []
-    labels = []
-    for filename in os.listdir(directory):
-        if filename.endswith('.jpg') or filename.endswith('.png'):
-            image = cv2.imread(os.path.join(directory, filename))
-            image = cv2.resize(image, (128, 128))
-            images.append(image)
-            label = filename.split('_')[0]
-            labels.append(label)
-    return np.array(images), np.array(labels)
-
-train_images, train_labels = read_and_preprocess_images(train_dir)
-
-test_images, test_labels = read_and_preprocess_images(test_dir)
-
-print("Train images shape:", train_images.shape)
-print("Train labels shape:", train_labels.shape)
-print("Test images shape:", test_images.shape)
-print("Test labels shape:", test_labels.shape)
-
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
@@ -49,6 +20,40 @@ model = models.Sequential([
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+dataset_dir = os.path.join(current_dir, 'dataset\\train')
+dataset_test_dir = os.path.join(current_dir, 'dataset\\test')
+classes = ['dogs', 'cats']
+train_images = []
+train_labels = []
+test_images = []
+test_labels = []
+target_size = (28, 28)
+
+for class_idx, class_name in enumerate(classes):
+    class_dir = os.path.join(dataset_dir, class_name)
+    for filename in os.listdir(class_dir):
+        image_path = os.path.join(class_dir, filename)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read image in grayscale
+        resized_image = cv2.resize(image, target_size)
+        train_images.append(resized_image.reshape((*target_size, 1)))  # Reshape to (28, 28, 1)
+        train_labels.append(class_idx)
+
+train_images = np.array(train_images)
+train_labels = np.array(train_labels)
+
+for class_idx, class_name in enumerate(classes):
+    class_dir = os.path.join(dataset_test_dir, class_name)
+    for filename in os.listdir(class_dir):
+        image_path = os.path.join(class_dir, filename)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read image in grayscale
+        resized_image = cv2.resize(image, target_size)
+        test_images.append(resized_image.reshape((*target_size, 1)))  # Reshape to (28, 28, 1)
+        test_labels.append(class_idx)
+
+test_images = np.array(test_images)
+test_labels = np.array(test_labels)
 
 model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
