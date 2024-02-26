12876f0524436162fb29e35a7c1f8bb240a9cae6
mayur <mayurpatkar68@gmail.com>
2024-02-26 20:42:41 +0530

    update

--- image recognition/dog-cat.py
+++ image recognition/dog-cat.py
@@ -17,27 +17,26 @@ model = models.Sequential([
     layers.Dense(1, activation='sigmoid')
 ])
 
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 dataset_dir = os.path.join(current_dir, 'dataset\\train')
 dataset_test_dir = os.path.join(current_dir, 'dataset\\test')
+
 classes = ['dogs', 'cats']
 train_images = []
 train_labels = []
 test_images = []
 test_labels = []
-target_size = (28, 28)
 
 for class_idx, class_name in enumerate(classes):
     class_dir = os.path.join(dataset_dir, class_name)
     for filename in os.listdir(class_dir):
         image_path = os.path.join(class_dir, filename)
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read image in grayscale
-        resized_image = cv2.resize(image, target_size)
-        train_images.append(resized_image.reshape((*target_size, 1)))  # Reshape to (28, 28, 1)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        resized_image = cv2.resize(image, (28, 28))
+        train_images.append(resized_image.reshape((*resized_image.shape, 1)))
         train_labels.append(class_idx)
 
 train_images = np.array(train_images)
@@ -47,17 +46,25 @@ for class_idx, class_name in enumerate(classes):
     class_dir = os.path.join(dataset_test_dir, class_name)
     for filename in os.listdir(class_dir):
         image_path = os.path.join(class_dir, filename)
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read image in grayscale
-        resized_image = cv2.resize(image, target_size)
-        test_images.append(resized_image.reshape((*target_size, 1)))  # Reshape to (28, 28, 1)
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        resized_image = cv2.resize(image, (28, 28))
+        test_images.append(resized_image.reshape((*resized_image.shape, 1)))
         test_labels.append(class_idx)
 
 test_images = np.array(test_images)
 test_labels = np.array(test_labels)
 
 model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
-
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print('Test accuracy:', test_acc)
-
 predictions = model.predict(test_images)
+
+# Example: Predicting a single image
+input_image = cv2.imread('dog.jpg', cv2.IMREAD_GRAYSCALE)
+resized_image = cv2.resize(input_image, (28, 28))
+normalized_image = resized_image / 255.0
+input_data = np.expand_dims(normalized_image, axis=0)
+
+predictions = model.predict(input_data)
+predicted_animal = 'dog' if predictions[0][0] > 0.5 else 'cat'
+print("Predicted animal:", predicted_animal)
